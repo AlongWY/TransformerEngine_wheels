3499502a7cbb11d0387b30596013b8c5fea97f26
ylfeng <ylfeng@ir.hit.edu.cn>
2024-05-09 22:35:44 +0800

    fix nvtx package

--- prepare_for_build.sh
+++ prepare_for_build.sh
@@ -1,5 +1,5 @@
 pip install setuptools==68.0.0 wheel auditwheel-symbols
-pip install ninja cmake
+pip install ninja cmake numpy
 # We want to figure out the CUDA version to download pytorch
 # e.g. we can have system CUDA version being 11.7 but if torch==1.12 then we need to download the wheel from cu116
 # see https://github.com/pytorch/pytorch/blob/main/RELEASE.md#release-compatibility-matrix
@@ -8,10 +8,6 @@ export TORCH_CUDA_VERSION=$(python -c "from os import environ as env; \
     minv = {'1.12': 113, '1.13': 116, '2.0': 117, '2.1': 118, '2.2': 118, '2.3': 118}[env['MATRIX_TORCH_VERSION']]; \
     maxv = {'1.12': 116, '1.13': 117, '2.0': 118, '2.1': 121, '2.2': 121, '2.3': 121}[env['MATRIX_TORCH_VERSION']]; \
     print(max(min(int(env['MATRIX_CUDA_VERSION']), maxv), minv))" \
-)
-export WHEEL_PYTHON_VERSION=$(python -c "from os import environ as env; \
-    version = {'37': 'cp37-cp37m', '38': 'cp38-cp38', '39': 'cp39-cp39', '310': 'cp310-cp310', '311': 'cp311-cp311'}[env['MATRIX_PYTHON_VERSION']]; \
-    print(version)" \
 )
 python --version
 pip --version
