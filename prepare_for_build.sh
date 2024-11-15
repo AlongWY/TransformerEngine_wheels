229dc245178eed84a8adf3abb22f138fe2c96458
Feng Yunlong <20281571+AlongWY@users.noreply.github.com>
2024-11-15 18:33:05 +0800

    Update prepare_for_build.sh

--- prepare_for_build.sh
+++ prepare_for_build.sh
@@ -1,12 +1,11 @@
-pip install setuptools==68.0.0 wheel auditwheel-symbols
-pip install ninja cmake "numpy<2.0.0"
+pip install -U setuptools wheel auditwheel-symbols ninja cmake "numpy<2.0.0"
 # We want to figure out the CUDA version to download pytorch
 # e.g. we can have system CUDA version being 11.7 but if torch==1.12 then we need to download the wheel from cu116
 # see https://github.com/pytorch/pytorch/blob/main/RELEASE.md#release-compatibility-matrix
 # This code is ugly, maybe there's a better way to do this.
 export TORCH_CUDA_VERSION=$(python -c "from os import environ as env; \
-    minv = {'1.12': 113, '1.13': 116, '2.0': 117, '2.1': 118, '2.2': 118, '2.3': 118, '2.4': 118}[env['MATRIX_TORCH_VERSION']]; \
-    maxv = {'1.12': 116, '1.13': 117, '2.0': 118, '2.1': 121, '2.2': 121, '2.3': 121, '2.4': 124}[env['MATRIX_TORCH_VERSION']]; \
+    minv = {'1.12': 113, '1.13': 116, '2.0': 117, '2.1': 118, '2.2': 118, '2.3': 118, '2.4': 118, '2.5': 118}[env['MATRIX_TORCH_VERSION']]; \
+    maxv = {'1.12': 116, '1.13': 117, '2.0': 118, '2.1': 121, '2.2': 121, '2.3': 121, '2.4': 124, '2.5': 124}[env['MATRIX_TORCH_VERSION']]; \
     print(max(min(int(env['MATRIX_CUDA_VERSION']), maxv), minv))" \
 )
 
