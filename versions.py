c20654434e888a175536ba009c52836aede5ee1d
Feng Yunlong <20281571+AlongWY@users.noreply.github.com>
2025-03-20 02:08:27 +0800

    Update versions.py

--- versions.py
+++ versions.py
@@ -1,9 +1,9 @@
 versions = {
-    "2.2.2": {"python": [9, 10, 11], "cuda": [121], "CXX": [False, False]},
-    "2.3.1": {"python": [9, 10, 11], "cuda": [121], "CXX": [False, False]},
-    "2.4.1": {"python": [9, 10, 11], "cuda": [121, 124], "CXX": [False, False]},
-    "2.5.1": {"python": [9, 10, 11, 12], "cuda": [121, 124], "CXX": [False, False]},
-    "2.6.0": {"python": [9, 10, 11, 12], "cuda": [124, 126], "CXX": [False, True]},
+    "2.2.2": {"python": [9, 10, 11], "cuda": [121]},
+    "2.3.1": {"python": [9, 10, 11], "cuda": [121]},
+    "2.4.1": {"python": [9, 10, 11], "cuda": [121, 124]},
+    "2.5.1": {"python": [9, 10, 11, 12], "cuda": [121, 124]},
+    "2.6.0": {"python": [9, 10, 11, 12], "cuda": [124, 126]},
 }
 
 cuda_version_mapping = {
@@ -17,9 +17,9 @@ pairs = []
 for torch_version, pycu in versions.items():
     for python_version in pycu["python"]:
         python_version = f"3.{python_version}"
-        for cuda_version, cxx11abi in zip(pycu["cuda"], pycu["CXX"]):
+        for cuda_version, cxx11abi in pycu["cuda"]:
             cuda_version = cuda_version_mapping[cuda_version]
-            pair = (torch_version, python_version, cuda_version, str(cxx11abi).upper())
+            pair = (torch_version, python_version, cuda_version)
             if pair not in pairs_set:
                 pairs.append(pair)
                 pairs_set.add(pair)
@@ -28,4 +28,3 @@ for torch_version, python_version, cuda_version, cxx11abi in pairs:
     print(f"- torch-version: \"{torch_version}\"")
     print(f"  python-version: \"{python_version}\"")
     print(f"  cuda-version: \"{cuda_version}\"")
-    print(f"  cxx11abi: {cxx11abi}")
