f7d778aa8abac6edb10077cbccca4f505632ae28
Feng Yunlong <20281571+AlongWY@users.noreply.github.com>
2025-06-02 15:30:09 +0000

    update to te v2.3

--- versions.py
+++ versions.py
@@ -4,12 +4,14 @@ versions = {
     "2.4.1": {"python": [9, 10, 11], "cuda": [121, 124]},
     "2.5.1": {"python": [9, 10, 11, 12], "cuda": [121, 124]},
     "2.6.0": {"python": [9, 10, 11, 12], "cuda": [124, 126]},
+    "2.7.0": {"python": [9, 10, 11, 12], "cuda": [124, 128]},
 }
 
 cuda_version_mapping = {
     121: "12.1.1",
     124: "12.4.0",
     126: "12.6.0",
+    128: "12.8.0",
 }
 
 pairs_set = set()
@@ -19,14 +21,12 @@ for torch_version, pycu in versions.items():
         python_version = f"3.{python_version}"
         for cuda_version in pycu["cuda"]:
             cuda_version = cuda_version_mapping[cuda_version]
-            for cxx11abi in [True, False]:
-                pair = (torch_version, python_version, cuda_version, str(cxx11abi).upper())
-                if pair not in pairs_set:
+            pair = (torch_version, python_version, cuda_version)
+            if pair not in pairs_set:
                     pairs.append(pair)
                     pairs_set.add(pair)
 
-for torch_version, python_version, cuda_version, cxx11abi in pairs:
+for torch_version, python_version, cuda_version in pairs:
     print(f"- torch-version: \"{torch_version}\"")
     print(f"  python-version: \"{python_version}\"")
     print(f"  cuda-version: \"{cuda_version}\"")
-    print(f"  cxx11abi: {cxx11abi}")
