dd4f485ceacadb7e9cb23bd0804038c8088826b6
Feng Yunlong <20281571+AlongWY@users.noreply.github.com>
2025-11-02 09:28:43 +0000

    2.0 rebuild for 2.9

--- versions.py
+++ versions.py
@@ -2,13 +2,14 @@
 import yaml
 
 versions = {
-    "2.2.2": {"python": [8, 9, 10, 11], "cuda": [121], "deepcompile": 0},
-    "2.3.1": {"python": [8, 9, 10, 11], "cuda": [121], "deepcompile": 0},
-    "2.4.1": {"python": [8, 9, 10, 11], "cuda": [121, 124], "deepcompile": 0},
-    "2.5.1": {"python": [9, 10, 11, 12], "cuda": [121, 124], "deepcompile": 0},
-    "2.6.0": {"python": [9, 10, 11, 12], "cuda": [124, 126], "deepcompile": 1},
-    "2.7.1": {"python": [9, 10, 11, 12, 13], "cuda": [126, 128], "deepcompile": 1},
-    "2.8.0": {"python": [9, 10, 11, 12, 13], "cuda": [126, 128, 129], "deepcompile": 1},
+    "2.9.0": {"python": [10, 11, 12, 13], "cuda": [126, 128, 130]},
+    "2.8.0": {"python": [9, 10, 11, 12, 13], "cuda": [126, 128, 129]},
+    "2.7.1": {"python": [9, 10, 11, 12, 13], "cuda": [126, 128]},
+    "2.6.0": {"python": [9, 10, 11, 12], "cuda": [124, 126]},
+    "2.5.1": {"python": [9, 10, 11, 12], "cuda": [121, 124]},
+    "2.4.1": {"python": [8, 9, 10, 11], "cuda": [121, 124]},
+    "2.3.1": {"python": [8, 9, 10, 11], "cuda": [121]},
+    "2.2.2": {"python": [8, 9, 10, 11], "cuda": [121]},
 }
 
 cuda_version_mapping = {
@@ -21,6 +22,7 @@ cuda_version_mapping = {
     126: "12.6.0",
     128: "12.8.0",
     129: "12.9.0",
+    130: "13.0.0",
 }
 
 cuda_arch = {
@@ -50,7 +52,6 @@ for torch_version, python_version, cuda_version, raw_cuda_version in pairs:
     print(f'  python-version: "{python_version}"')
     print(f'  cuda-version: "{cuda_version}"')
     print(f'  arch: "{cuda_arch[cuda_version]}"')
-    print(f"  deepcompile: {versions[torch_version]['deepcompile']}")
 
     print(f'  cibw-build: "cp{python_version.replace(".", "")}-*64"')
     print(f'  cibw-build-image: "pytorch/manylinux2_28-builder:cuda{cuda_version[:-2]}"')
